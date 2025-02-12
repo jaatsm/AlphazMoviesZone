7f72e7ee7600567c58656c588bddf3416d1f50dd
jaatsm <sourabhmahala930@gmail.com>
2025-02-12 13:00:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -67,7 +67,7 @@ STREAM_MODE = is_enabled((environ.get('STREAM_MODE', "False")), False)
 
 
 #verify site api and url
-IS_VERIFY = is_enabled((environ.get('IS_VERIFY', 'False')), False)
+IS_VERIFY = is_enabled((environ.get('IS_VERIFY', 'True')), False)
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
 VERIFY_URL = environ.get('VERIFY_URL', 'shortxlinks.com')
 VERIFY_API = (environ.get('VERIFY_API', '7565b54ead36267a2a570fe3ab7f4fd39ebe5450'))
